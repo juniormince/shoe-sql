61e8d3852e21b0ec0d6aae8d15283cf71f1627a0
juniormince <groundcontroltomajorbree@gmail.com>
2018-05-03 13:18:48 -0500

    add angular dom connection

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,27 @@ console.log('js czeck');
 
 var app = angular.module('ShoeApp', []);
 
-app.controller('ShoeController', function() {
+app.controller('ShoeController', ['$http', function($http) {
     console.log('ShoeController czeck');
-});
+
+    var self = this;
+    self.message = 'msg czeck one two';
+
+    self.getShoe = $http({
+        method: 'GET',
+        url: '/shoe'
+    })
+    
+    .then(function ()   {
+        console.log('get the things');
+    })
+    .catch(function(error)  {
+        console.log('sorry got an error', error);
+    });
+
+
+
+
+
+
+}]);
