4b3dfe0530fd0f4fc886d61a55134c8e1936a856
juniormince <groundcontroltomajorbree@gmail.com>
2018-05-03 11:11:53 -0500

    added postgres connection

--- server/server.js
+++ server/server.js
@@ -1,27 +1,50 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const pg = require('pg');
+
+const Pool = pg.Pool;
+
+const pool = new Pool({
+    database: 'shoe_store', //the name of the database
+    host: 'localhost', //where is your database?
+    port: 5432, //port of database, 5432 = default for postgres
+    max: 10, //how many connections (queries) at one time
+    idleTimeoutMillis: 30000, //30 seconds to try to connect, otherwise cancel query
+});
+
+pool.on('connect', () => {
+    console.log('postgresql connected');
+
+});
+
+pool.on('error', (error) => {
+    console.log('error with postgres pool', error);
+})
+
 
 const app = express();
-
-const bodyParser = require('body-parser');
-
 const PORT = process.env.PORT || 5000;
-
 app.use(bodyParser.json());
 
 
-////////////
- 
-////////////
-
 app.get('/shoe', function (req, res) {
     console.log('get thing');
-    res.send
+    res.send(Pool);
 });
 
 
 app.post('/shoe', function (req, res) {
-    console.log('post thing', req.body);
-    res.sendStatus(200);
+    const shoe = req.body;
+    pool.query(`INSERT INTO "shoes" ("name", "cost")
+                VALUES ($1, $2);`, [shoe.name, shoe.cost])
+        .then((results) => {
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.log('error with SQL INSERT on shoe POST', error);
+            res.sendStatus(500);
+        });
+
 });
 
 
